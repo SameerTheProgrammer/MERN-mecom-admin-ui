35fa06d1cce1e1a65eaa1face0ae13dcc80794f2
Sameer kumar <2006Sameer2009@gmail.com>
2024-02-11 23:12:14 +0530

    login form refactoring

--- src/pages/login/Login.Page.tsx
+++ src/pages/login/Login.Page.tsx
@@ -17,13 +17,6 @@ const { Title, Paragraph } = Typography;
 const LoginPage = () => {
     return (
         <>
-            {/* <div>Sign in</div>
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Log in</button>
-            <label htmlFor="remember-me">Remember me</label>
-            <input type="checkbox" id="remember-me" />
-            <a href="#">Forgot Password</a> */}
             <Layout
                 style={{
                     height: "100vh",
