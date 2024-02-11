1542e9478dce8886c9fa9d6f2c93a4b43ba2865c
Sameer kumar <2006Sameer2009@gmail.com>
2024-02-11 22:03:57 +0530

    login component complete

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "antd/dist/reset.css";
 import "./index.css";
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
+import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: "#4ED93F",
+                    colorLink: "#3FD99B",
+                },
+            }}
+        >
+            <RouterProvider router={router} />
+        </ConfigProvider>
     </React.StrictMode>,
 );
