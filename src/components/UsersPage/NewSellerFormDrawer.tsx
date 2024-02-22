8917ff65ab05e3a305c2be92ebf5f02dd6b56dcd
Sameer kumar <2006Sameer2009@gmail.com>
2024-02-22 20:13:13 +0530

    create basic form for add seller

--- src/components/UsersPage/NewSellerFormDrawer.tsx
+++ src/components/UsersPage/NewSellerFormDrawer.tsx
@@ -1,5 +1,15 @@
 import React, { Dispatch, SetStateAction } from "react";
-import { Button, Drawer, Space } from "antd";
+import {
+    Button,
+    Card,
+    Col,
+    Drawer,
+    Form,
+    Input,
+    Row,
+    Space,
+    theme,
+} from "antd";
 
 interface NewSellerFormDrawerProps {
     drawerOpen: boolean;
@@ -14,6 +24,10 @@ const NewSellerFormDrawer: React.FC<NewSellerFormDrawerProps> = ({
         setDrawerOpen(false);
     };
 
+    const {
+        token: { colorBgLayout },
+    } = theme.useToken();
+
     return (
         <>
             <Drawer
@@ -22,6 +36,7 @@ const NewSellerFormDrawer: React.FC<NewSellerFormDrawerProps> = ({
                 onClose={onClose}
                 open={drawerOpen}
                 destroyOnClose={true}
+                styles={{ body: { background: colorBgLayout } }}
                 extra={
                     <Space>
                         <Button onClick={onClose}>Cancel</Button>
@@ -29,9 +44,95 @@ const NewSellerFormDrawer: React.FC<NewSellerFormDrawerProps> = ({
                     </Space>
                 }
             >
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+                <Form layout="vertical">
+                    <Row gutter={[16, 16]}>
+                        <Col span={24}>
+                            <Card
+                                bordered={false}
+                                title={"Basic info"}
+                                // style={{ width: 300 }}
+                            >
+                                <Row>
+                                    <Col span={24}>
+                                        <Form.Item
+                                            label="Name"
+                                            required
+                                            name="name"
+                                        >
+                                            <Input />
+                                        </Form.Item>
+                                    </Col>
+                                    <Col span={24}>
+                                        <Row gutter={16}>
+                                            <Col span={12}>
+                                                <Form.Item
+                                                    label="Email"
+                                                    required
+                                                    name="email"
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col span={12}>
+                                                <Form.Item
+                                                    label="Phone Number"
+                                                    required
+                                                    name="phoneNumber"
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </Card>
+                        </Col>
+                        <Col span={24}>
+                            <Card
+                                bordered={false}
+                                title={"Additional info"}
+                                // style={{ width: 300 }}
+                            >
+                                <Row>
+                                    <Col span={24}>
+                                        <Form.Item
+                                            label="Description"
+                                            required
+                                            name="description"
+                                        >
+                                            <Input.TextArea
+                                                showCount
+                                                maxLength={255}
+                                            />
+                                        </Form.Item>
+                                    </Col>
+                                    <Col span={24}>
+                                        <Row gutter={16}>
+                                            <Col span={12}>
+                                                <Form.Item
+                                                    label="Email"
+                                                    required
+                                                    name="email"
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col span={12}>
+                                                <Form.Item
+                                                    label="Phone Number"
+                                                    required
+                                                    name="phoneNumber"
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Form>
             </Drawer>
         </>
     );
